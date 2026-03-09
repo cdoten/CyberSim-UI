d0c2dc211786548197e796f4cc172427b3dd0ff7
Chris Doten <chris@doten.us>
2026-03-09 15:31:43 -0400

    Removing block on socket and debugging connections.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import qs from 'query-string';
 import { view } from '@risingstack/react-easy-state';
 import { Spinner } from 'react-bootstrap';
@@ -15,6 +16,17 @@ const queryParams = qs.parse(window.location.search);
 const Game = view(() => {
   const { state: gameState, socketConnected } = gameStore;
   const { loading: loadingStaticData } = useStaticData();
+
+   useEffect(() => {
+      gameStore.ensureSocket();
+    }, []);
+
+  console.log('GAME STATE', {
+    loadingStaticData,
+    socketConnected,
+    gameState,
+    gameStoreLoading: gameStore.loading,
+  });
 
   if (loadingStaticData || !socketConnected) {
     return (
